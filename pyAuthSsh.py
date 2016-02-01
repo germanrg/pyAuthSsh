a4f1f93399467ac26ae7f1bb03661adab9640a27
gNrg <gnrg@tuta.io>
2016-02-01 11:47:41 +0100

    Add support for more auth.log entries.

--- pyAuthSsh.py
+++ pyAuthSsh.py
@@ -45,8 +45,9 @@ def show_menu_options():
 	print(Fore.YELLOW + "\t1 - )   " + Fore.RESET + "Show all SSH servers started in this host")
 	print(Fore.YELLOW + "\t2 - )   " + Fore.RESET + "Show all accepted passwords and opened sessiones")
 	print(Fore.YELLOW + "\t3 - )   " + Fore.RESET + "Show all closed sessions")
-	print(Fore.YELLOW + "\t4 - )   " + Fore.RESET + "Show all ssh failed authentications ")
-	print(Fore.YELLOW + "\t5 - )   " + Fore.RESET + "Exit")
+	print(Fore.YELLOW + "\t4 - )   " + Fore.RESET + "Show all ssh failed authentications")
+	print(Fore.YELLOW + "\t5 - )   " + Fore.RESET + "Show all ssh not received identifications")
+	print(Fore.YELLOW + "\t6 - )   " + Fore.RESET + "Exit")
 	print("\n")
 	option = raw_input(Fore.YELLOW + Style.BRIGHT + "   Choose one of this options: ")
 	return option
@@ -63,17 +64,19 @@ def get_log():
 	return text
 
 # This function classify relevant lines on categories
-def classify_entries(sshd_lines, servers_listening, opened_sessions, closed_sessions, auth_failures):
+def classify_entries(sshd_lines, servers_listening, opened_sessions, closed_sessions, auth_failures, no_identifications):
 # Types of sshd lines analyzed:
 #	- Server listening [Date - host - ip - port]
 #	- Accepted password + sshd:session
 #	- Received disconnect + sshd:session
 #	- sshd:auth authentication failure [LogName - uid - euid - tty - ruser - rhost - user ]
+#   - NEW: sshd[7748]: Did not receive identification string from X.X.X.X
 	for entry in sshd_lines:
 		if entry.find("Server listening") != -1: servers_listening.append(entry)
 		if entry.find("Accepted password") != -1: opened_sessions.append(entry)
 		if entry.find("Received disconnect") != -1: closed_sessions.append(entry)
 		if entry.find("pam_unix(sshd:auth): authentication failure") != -1: auth_failures.append(entry)
+		if entry.find("Did not receive identification") != -1: no_identifications.append(entry)
 
 # This function shows local ssh daemons runned logged in /var/auth/auth.log
 def get_servers(servers_listening):
@@ -94,6 +97,9 @@ def get_servers(servers_listening):
 
 # This function shows failed authentications logged in /var/auth/auth.log
 def get_auth_fails(auth_failures):
+	''' Example auth.log line:
+	Jan 31 16:53:59 nb200 sshd[15920]: pam_unix(sshd:auth): authentication failure; logname= uid=0 euid=0 tty=ssh ruser= rhost=localhost  user=xxxx
+	'''
 	if len(auth_failures) > 0:
 		print("\t" + Back.GREEN + Style.BRIGHT + "  " + Back.RESET + "\tOK: authentication failures have been loaded.\n")
 	else: 
@@ -111,6 +117,9 @@ def get_auth_fails(auth_failures):
 
 # This function shows opened sessions logged in /var/auth/auth.log
 def get_opened_sessions(opened_sessions):
+	''' Example auth.log line:
+	Jan 31 16:53:48 nb200 sshd[15885]: Received disconnect from 127.0.0.1: 11: disconnected by user
+	'''
 	if len(opened_sessions) > 0:
 		print("\t" + Back.GREEN + Style.BRIGHT + "  " + Back.RESET + "\tOK: Opened sessions have been loaded.\n")
 	else: 
@@ -127,6 +136,9 @@ def get_opened_sessions(opened_sessions):
 
 # This function shows opened sessions logged in /var/auth/auth.log
 def get_closed_sessions(closed_sessions):
+	''' Example auth.log line:
+	Jan 31 16:53:48 nb200 sshd[15885]: Received disconnect from 127.0.0.1: 11: disconnected by user
+	'''
 	if len(closed_sessions) > 0:
 		print("\t" + Back.GREEN + Style.BRIGHT + "  " + Back.RESET + "\tOK: Closed sessions have been loaded.\n")
 	else: 
@@ -140,7 +152,26 @@ def get_closed_sessions(closed_sessions):
 		info = "\tDisconnected by: " + str(fields[12]) + "\tIP: " + str(fields[8]) + "\n"
 		info += output
 		print info
-#Jan 31 16:53:48 nb200 sshd[15885]: Received disconnect from 127.0.0.1: 11: disconnected by user
+
+# This function shows identifications not received logged in /var/auth/auth.log
+def get_no_identification(no_identifications):
+	''' Example auth.log line:
+	Aug 19 06:28:43 izxvps sshd[5838]: Did not receive identification string from 59.151.37.10
+	'''
+	if len(no_identifications) > 0:
+		print("\t" + Back.GREEN + Style.BRIGHT + "  " + Back.RESET + "\tOK: No received identifications have been loaded.\n")
+	else: 
+		print("\t" + Back.RED + Style.BRIGHT + "  " + Back.RESET + "\tUps. It seems like there is no received identifications.\n")
+
+	for identification in no_identifications:
+		output = '\t'
+		fields = identification.split(" ")
+		date = str(fields[2]) + " - " + str(fields[1]) + "/" + str(months[fields[0]])
+		output += 'Log date:\t' + date + "\n"
+		info = "\tDid not receive identification string from:\t" + str(fields[11]) + "\n"
+		info += output
+		print info
+
 
 if __name__ == "__main__":
 
@@ -159,8 +190,8 @@ if __name__ == "__main__":
 	opened_sessions = []
 	closed_sessions = []
 	auth_failures = []
-
-	classify_entries(sshd_lines, servers_listening, opened_sessions, closed_sessions, auth_failures)
+	no_identifications = []
+	classify_entries(sshd_lines, servers_listening, opened_sessions, closed_sessions, auth_failures, no_identifications)
 
 	option = '0'
 	while option:
@@ -171,16 +202,17 @@ if __name__ == "__main__":
 		elif option == "2": get_opened_sessions(opened_sessions)
 		elif option == "3": get_closed_sessions(closed_sessions)
 		elif option == "4": get_auth_fails(auth_failures)
-		elif option == "5": print("\t" + Back.GREEN + Style.BRIGHT + "  " + Back.RESET + "\tThanks for using. Bye!\n\n\t" + Back.BLUE + "  " + Back.RESET + "\tgnrg@tuta.io\n\n"); break
+		elif option == "5": get_no_identification(no_identifications)
+		elif option == "6": print("\t" + Back.GREEN + Style.BRIGHT + "  " + Back.RESET + "\tThanks for using. Bye!\n\n\t" + Back.BLUE + "  " + Back.RESET + "\tgnrg@tuta.io\n\n"); break
 		else:
 			print("\t" + Back.RED + Style.BRIGHT + "  " + Back.RESET + "\tIncorrect option. Try again!\n")
 			option = '0'
 
 """
-Jan 31 16:53:59 nb200 sshd[15920]: pam_unix(sshd:auth): authentication failure; logname= uid=0 euid=0 tty=ssh ruser= rhost=localhost  user=gnrg
 Jan 31 16:54:01 nb200 sshd[15920]: Failed password for gnrg from 127.0.0.1 port 38861 ssh2
 Jan 31 16:54:07 nb200 sshd[15920]: message repeated 2 times: [ Failed password for gnrg from 127.0.0.1 port 38861 ssh2]
 Jan 31 16:54:07 nb200 sshd[15920]: Connection closed by 127.0.0.1 [preauth]
 Jan 31 16:54:07 nb200 sshd[15920]: PAM 2 more authentication failures; logname= uid=0 euid=0 tty=ssh ruser= rhost=localhost  user=gnrg
-
 """
+
+# Read about use of fail2ban for the detected intrussion attempts.
